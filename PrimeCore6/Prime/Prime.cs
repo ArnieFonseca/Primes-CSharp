fb9bd6e7296167626f22aca30474b22e3c03cf56
Arnie Fonseca <arniefonseca@hotmail.com>
2021-11-18 07:53:31 -0500

    Initial Setup

---
+++ PrimeCore6/Prime/Prime.cs
@@ -0,0 +1,47 @@
+﻿ 
+namespace Prime ;
+ 
+public static class Calculator
+{
+    #region Constant
+    const int ZERO = 0; 
+    #endregion
+
+    #region Functions
+    /// <summary>
+    /// Obtain the List of Primes Numbers
+    /// </summary>
+    public static List<int> GetPrimes(List<int> numbers)
+    {
+        List<int> rst = new();
+        return GetPrimesHelper(numbers, rst);
+    }
+
+    /// <summary>
+    /// Tail Recursive Helper function to get all the Primes Number fro the Original Sequence
+    /// </summary>
+    private static List<int> GetPrimesHelper(List<int> numbers, List<int> rst)
+    {
+        return (numbers.Count == ZERO)
+            ? rst
+            : GetPrimesHelper(RemoveFactor(numbers), AddPrime(numbers, rst))
+            ;
+    }
+
+    /// <summary>
+    /// Filter the List/Array deleting all factors of the List Head
+    /// </summary>
+    private static List<int> RemoveFactor(List<int> numbers)
+    {
+        return numbers.Where(n => n % numbers.First() != ZERO).ToList();
+    }
+
+    /// <summary>
+    /// The Head of the List/Array is added to the resulting List
+    /// </summary>
+    private static List<int> AddPrime(List<int> numbers, List<int> rst)
+    {
+        return rst.Append(numbers.First()).ToList();
+    }
+    #endregion
+}
---
+++ PrimeCore6/Program.cs
@@ -0,0 +1,22 @@
+﻿
+#region Constants
+const int ONE = 1;
+const int TWO = 2;
+const int LIMIT = 20;
+#endregion
+
+#region Main Logic
+// Create consequent integers starting with two until Limit 
+var numbers = Enumerable.Range(TWO, LIMIT - ONE).ToList();
+
+// Generate Primes Numbers from sequence
+var result = Prime.Calculator.GetPrimes(numbers);
+
+// Display the Original List
+numbers.ForEach(n => Write($"{n} "));
+
+WriteLine();
+
+//Display the Prime Numbers
+result.ForEach(p => Write($"{p} "));
+#endregion
